e45aa6777b63af71c54857f1b59de53bc6d0f3ed
José Lucas Eloi <lucaseloideolibeira@gmail.com>
2025-12-10 18:13:05 -0300

    Corrigindo problema de model em exibição

--- model/bibliotecaModel.js
+++ model/bibliotecaModel.js
@@ -3,12 +3,15 @@ const conexao = require("../dados/conexoes");
 class atendimentoModel {
   listar() {
     const sql = "select * from livro";
-    return this.conexao.query(sql, {}, (error, resposta) => {
-      if (error) {
-        console.log("Erro no model");
-        return;
-      }
-      console.log("Deu certo com o model");
+    return new Promise((resolve, reject) => {
+      conexao.query(sql, {}, (error, resposta) => {
+        if (error) {
+          console.log("Erro no model");
+          reject(error);
+        }
+        console.log("Deu certo com o model");
+        resolve(resposta);
+      });
     });
   }
 }
